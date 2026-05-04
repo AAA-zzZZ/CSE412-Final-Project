7e93f5282a0b9a3fe7a7db4591e3f72ad96b9cfc
AAA-zzZZ <ezheng3@asu.edu>
2026-05-03 18:23:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -68,6 +68,56 @@ app.delete('/api/products/:id', async (req, res) => {
 
 //other crud goes here
 
+//merchant
+app.get('/api/merchants', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM Merchant');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//manufacturer
+app.get('/api/manufacturers', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM Manufacturer');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+//merchprod
+app.get('/api/merchprod', async (req, res) => {
+  try {
+    const result = await pool.query(`SELECT * from MerchProd `);
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  } 
+});
+
+//ratings
+app.get('/api/ratings', async (req, res) => {
+  try {
+    const result = await pool.query(`SELECT * from ratings`);
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  } 
+});
+
+// user
+app.get('/api/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM "users"');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 
 app.listen(5000, () => {
